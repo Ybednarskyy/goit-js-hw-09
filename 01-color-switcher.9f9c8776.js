a12ff907f0e5b0697bc17e585b4e24e891d10919
Ybednarskyy <y.bednarskyy@gmail.com>
2022-11-06 21:08:34 +0000

    Deploying to gh-pages from @ Ybednarskyy/goit-js-hw-09@35d5b202d2958fe418c09155d782430482dd65f6 🚀

---
+++ 01-color-switcher.9f9c8776.js
@@ -0,0 +1,2 @@
+const t={body:document.querySelector("body"),start:document.querySelector("[data-start]"),stop:document.querySelector("[data-stop]")};let e=null;function o(){t.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}t.start.addEventListener("click",(function(){e||(e=setInterval(o,1e3))})),t.stop.addEventListener("click",(function(){clearInterval(e),e=null}));
+//# sourceMappingURL=01-color-switcher.9f9c8776.js.map
---
+++ 01-color-switcher.f17ed6a2.js
@@ -0,0 +1,2 @@
+!function(){var t={body:document.querySelector("body"),start:document.querySelector("[data-start]"),stop:document.querySelector("[data-stop]")},e=null;function o(){t.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}t.start.addEventListener("click",(function(){e||(e=setInterval(o,1e3))})),t.stop.addEventListener("click",(function(){clearInterval(e),e=null}))}();
+//# sourceMappingURL=01-color-switcher.f17ed6a2.js.map

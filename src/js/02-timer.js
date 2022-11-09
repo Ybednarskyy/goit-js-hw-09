9411013ecfd1e4ca5564367a960fd427609ed81b
Ybednarskyy <y.bednarskyy@gmail.com>
2022-11-09 21:22:57 +0200

    Update 02-timer.js

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -17,3 +17,15 @@ const options = {
     console.log(selectedDates[0]);
   },
 };
+
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+import Notiflix from 'notiflix';
+
+// 1. Надо в функции вывода модалки описать текущую дату (selectedDates < текущей), и что ранняя дата это не правильно. (DONE)
+// 2. Надо определить как в text.Content рефов присваивать разницу между выбраной датой и текущей (DONE)
+// 2.1 Нужна проверка: Если разница между текущей датой и выбранной меньше 0, то в textContent рефов присваивается 00 (DONE)
+// 3. Надо определить как отсчет таймера запустить в обратную сторону. (DONE)
+// 4. Когда text.Content рефов = 00 то надо таймер остановить clearInterval(this.intervalId)
+
+const inputDayTime = document.querySelector('#datetime-picker');

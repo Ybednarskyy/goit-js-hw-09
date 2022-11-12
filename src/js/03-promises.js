658a6569002fe148ccf7cfdf497644aca3b8a12e
Ybednarskyy <y.bednarskyy@gmail.com>
2022-11-12 22:34:40 +0200

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,3 +1,18 @@
+const refs = {
+  form: document.querySelector('.form'),
+};
+
+const args = {};
+
+// console.log(refs.form);
+refs.form.addEventListener('change', e => {
+  // console.log('Target => ', e.target['name']);
+  // console.log('Value => ', e.target.value);
+  // console.log('Current target => ', e.currentTarget);
+  args[e.target.name] = e.target.value;
+  console.log(args);
+});
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
@@ -6,3 +21,22 @@ function createPromise(position, delay) {
     // Reject
   }
 }
+
+// const promise = new Promise((resolve, reject) => {
+//   const isFulFill = Math.random() > 0.5;
+//   setTimeout(() => {
+//     if (isFulFill) {
+//       resolve('Promise is Ok ;-))');
+//     }
+//     reject('Promise is not Ok :-[[');
+//   }, 2000);
+// });
+
+// promise.then(
+//   result => {
+//     console.log(result);
+//   },
+//   error => {
+//     console.log(error);
+//   }
+// );
